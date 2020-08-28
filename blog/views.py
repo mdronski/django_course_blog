c600a58467baa5a4bc7bc67b34e0cea7d47407be
mdronski <dronskimichal@gmail.com>
2020-08-28 21:31:55 +0200

    Create views for model classes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from blog.forms import PostForm
+from blog.models import Post
+
+
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects\
+            .filter(published_date__lte=timezone.now())\
+            .order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    model = Post
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+
+    def get_queryset(self):
+        return Post.objects\
+            .filter(published_date__isnull=True)\
+            .order_by('create_day')
