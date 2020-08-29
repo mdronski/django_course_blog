69dc0208d769109159d155ae098f754f8fd28aca
mdronski <dronskimichal@gmail.com>
2020-08-29 15:55:35 +0200

    Create comment views

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
     re_path(r'post/(?P<pk>\d+)/edit', views.PostUpdateView.as_view(), name='post_edit'),
     re_path(r'post/(?P<pk>\d+)/remove', views.PostDeleteView.as_view(), name='post_remove'),
+    re_path(r'post/(?P<pk>\d+)/publish', views.post_publish, name='post_publish'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    re_path(r'post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'comments/(?P<pk>\d+)/approve$', views.comment_approve, name='comment_approve'),
+    re_path(r'comments/(?P<pk>\d+)/remove$', views.comment_remove, name='comment_remove'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from blog.forms import PostForm
-from blog.models import Post
+from blog.forms import PostForm, CommentForm
+from blog.models import Post, Comment
 
 
 class AboutView(TemplateView):
@@ -16,8 +18,8 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects\
-            .filter(published_date__lte=timezone.now())\
+        return Post.objects \
+            .filter(published_date__lte=timezone.now()) \
             .order_by('-published_date')
 
 
@@ -50,6 +52,44 @@ class DraftListView(LoginRequiredMixin, ListView):
     redirect_field_name = 'blog/post_list.html'
 
     def get_queryset(self):
-        return Post.objects\
-            .filter(published_date__isnull=True)\
+        return Post.objects \
+            .filter(published_date__isnull=True) \
             .order_by('create_day')
+
+
+# check if could be replaced by class views
+@login_required
+def post_publish(request: HttpRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=post.pk)
+
+
+@login_required
+def add_comment_to_post(request: HttpRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+        return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request: HttpRequest, pk: int):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request: HttpRequest, pk: int):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    redirect('post_detail', pk=post_pk)
